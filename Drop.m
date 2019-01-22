bf76a36dcb9836e7c4313c4fc2d7450aa7261b01
Brendan Palmer <brendanpalmer@cu-engr2-1-10-10.201.37.148.int.colorado.edu>
2019-01-22 14:30:55 -0700

    change acceleration

--- Drop.m
+++ Drop.m
@@ -1,4 +1,4 @@
-function [ t v sy, sx, N] = Drop(v0, t0, theta, length, h0 )
+function [ t v sy, sx, N] = Drop(v0, t0, theta, length, sx0, sy0)
 % Downhill ramp function ASEN 2003 Lab 1
 % Position is given in standard x y coordinate system
 % velocity and acceleration follow a tilted x y coordiante system with x
@@ -15,25 +15,23 @@ RampHeight = length * sind(theta);
 RampWidth = length * cosd(theta);
 
 % return output
-sx = RampWidth;
-sy = RampHeight;
+sx = sx0 + RampWidth;
+sy = sy0 - RampHeight;
 
 % get final velocity based on height.
 syms h
-v(h) = sqrt ( 2 * g * (h0 - h)); 
+v(h) = sqrt ( 2 * g * (sy0 - h)); 
 
 %evaluate veloc function defined above.
-v = double(v(h0 - RampHeight));
+v = double(v(sy0 - RampHeight));
 
 %% time spent: use kinematic equations to get the time spent on the ramp
 
 %all acceleration is in the x direction due to the tilted plane
 t = ((v - v0) / ax) * t0;
 
-%% G's felt.
+%% G's
 
 N = sqrt(ax^2 + ax^2) / g; 
-fprintf('The Ramp down will make you feel: %6.2f %12.8f \n ', N)
-fprintf(' G \n ' );
 end
 
